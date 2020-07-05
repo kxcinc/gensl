22df38b2cd4a2284dafbd003fc192c0503f05593
Haochen Xie <haochenx@acm.org>
2020-07-06 08:01:14 +0900

    utils - improve performance of Queue

--- utils.ml
+++ utils.ml
@@ -1,14 +1,15 @@
 module Queue : sig
-  type 'x t = private 'x list
+  type 'x t
   val empty : 'x t
   val add : 'x t -> 'x -> 'x t
   val take : 'x t -> ('x * 'x t) option
 end = struct
-  type 'x t = 'x list
-  let empty = []
-  let add q x = x :: q
-  let take q = match List.rev q with
-    | hd :: rest -> Some (hd, List.rev rest)
-    | [] -> None
+  type 'x t = 'x list*'x list
+  let empty = [], []
+  let add (r,u) x = (x :: r, u)
+  let rec take (r,u) = match u, r with
+    | hd :: rest, _ -> Some (hd, (r, rest))
+    | [], (_ :: _) -> take ([], List.rev r)
+    | [], [] -> None
 end
 type 'x queue = 'x Queue.t
