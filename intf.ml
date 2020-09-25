71e9970a2d530af276f7e93d2a0a7b59cc437705
Xuanrui Qi <me@xuanruiqi.com>
2020-09-25 15:03:37 +0900

    Interface for Canonicaltree

--- intf.ml
+++ intf.ml
@@ -1,5 +1,6 @@
 type sexp = Ppx_sexp_conv_lib.Sexp.t
 open Gensl
+open Basetypes
 
 type formatter = Format.formatter
 type atom = Basetypes.atom
@@ -71,11 +72,126 @@ module type Treeflavor = sig
   val to_parsetree     : datum -> ptree
 end
 
+(* https://stackoverflow.com/questions/55522847/implementing-haskells-take-function-using-foldl *)
+let take (n: int) (xs: 'a list): 'a list =
+  if n = 0
+  then []
+  else
+    let go r x i = if i >= n then [] else x :: r (i+1) in
+    List.fold_left go (fun _ -> []) xs 0
+      
+let drop (n: int) (xs: 'a list): 'a list =
+  if n = 0
+  then xs
+  else
+    let go r _ i xs = if i >= n then xs else
+        (match xs with _ :: t ->  r (i+1) t | _ -> failwith "Error") in
+    List.fold_left go (fun _ _ -> []) xs 0 xs
+
+let update (n: int) (x: 'a) (xs: 'a list): 'a list =
+  take n xs @ x :: drop n xs
+
+let remove (n: int) (xs: 'a list): 'a list =
+  take n xs @ drop n xs
+
 (* TASK 1 *)
 (* module CanonicaltreeFlavor : Treeflavor = struct
  *   (\* something here *\)
  * end *)
 (* and for the other trees *)
+module CanonicaltreeFlavor : Treeflavor = struct
+  open Canonicaltree
+  type datum = Canonicaltree.cdatum
+  (*
+    | CAtom of atom
+    | CForm of {
+        ckwd : (cdatum, cdatum) assoc;
+        cpos : cdatum list;
+      } *)
+      
+  type flavor = ctree
+      
+  let treeflavor : ctree treeflavor = Ctree
+
+  let compare : datum -> datum -> int = cdatum_ordering
+  let eqv c c': bool = cdatum_ordering c c' = 0
+  let pp : formatter -> datum -> unit = pp_cdatum
+  let to_string : ?pretty:bool -> datum -> string = [%noimplval]
+  let datum_of_sexp : sexp -> datum = [%noimplval]
+  let sexp_of_datum : datum -> sexp = [%noimplval]
+  
+  (** destructors *)
+
+  let atom dat =
+    match dat with
+    | CAtom a -> a
+    | _ -> failwith "Not an atom!"
+             
+  let npos ~pos dat =
+    match dat with
+    | CForm { ckwd = _; cpos = poses } ->
+      List.nth poses pos
+    | _ -> failwith "No positional arguments!"
+  
+  let kval ~key dat =
+    match dat with
+    | CForm { ckwd = kws; cpos = _ } ->
+      (match List.assoc_opt key kws with
+       | Some v -> v
+       | None -> failwith "Invalid key!")
+    | _ -> failwith "No keyword arguments!"
+  
+  let root : datum -> datum = [%noimplval]
+  (* No annotations ath the Canonicaltree level *)
+  let anno : datum -> datum list = fun _ -> []
+  
+  (** constructors & case analyzer *)
+
+  let mkatom : atom -> datum = fun a -> CAtom a
+  let mkform ~keyworded ~positional =
+    CForm {ckwd = keyworded; cpos = positional}
+  (* val case : atom:(datum -> 'r) ->
+             form:(keyworded:(datum*datum) list ->
+                   positional:datum list ->
+                   'r) ->
+             datum -> 'r*)
+  let case ~atom ~form d =
+    match d with
+    | CAtom _ -> atom d
+    | CForm {ckwd; cpos} ->
+      form ~keyworded:ckwd ~positional:cpos
+
+  (** updaters *)
+
+  let update_atom a dat =
+    match dat with
+    | CAtom _ -> CAtom a
+    | _ -> dat
+
+  let update_npos ~pos (nd: datum option) (dat: datum): datum =
+    match dat with
+    | CAtom _ -> dat
+    | CForm {ckwd; cpos} ->
+      (match nd with
+       | Some n -> CForm {ckwd = ckwd; cpos = update pos n cpos}
+       | _ -> CForm {ckwd = ckwd; cpos = remove pos cpos})
+      
+      
+  let update_kval : key:datum -> datum option -> datum -> datum = [%noimplval]
+  let update_root : datum -> datum -> datum = [%noimplval]
+  let update_anno : datum list -> datum -> datum = fun _ dat -> dat
+
+  (** converters *)
+
+  let to_canonicaltree : datum -> ctree = fun d -> d
+  let to_normaltree    : datum -> ntree = Normaltree.ndatum_of_cdatum
+  let to_datatree      : datum -> dtree =
+    composite Datatree.ddatum_of_ndatum Normaltree.ndatum_of_cdatum 
+  let to_parsetree     : datum -> ptree =
+    composite Parsetree.pdatum_of_ddatum (composite Datatree.ddatum_of_ndatum Normaltree.ndatum_of_cdatum)
+      
+end
+
 
 module type Zipperlib = functor (Flavor : Treeflavor) -> sig
   type t
