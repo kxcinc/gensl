8da5f088adefb9a0aae750e929c2bab6cb9d844e
Xuanrui Qi <me@xuanruiqi.com>
2020-10-05 01:44:56 +0900

    Datatree

--- intf.ml
+++ intf.ml
@@ -72,27 +72,15 @@ module type Treeflavor = sig
   val to_parsetree     : datum -> ptree
 end
 
-(* https://stackoverflow.com/questions/55522847/implementing-haskells-take-function-using-foldl *)
-let take (n: int) (xs: 'a list): 'a list =
-  if n = 0
-  then []
-  else
-    let go r x i = if i >= n then [] else x :: r (i+1) in
-    List.fold_left go (fun _ -> []) xs 0
-      
-let drop (n: int) (xs: 'a list): 'a list =
-  if n = 0
-  then xs
-  else
-    let go r _ i xs = if i >= n then xs else
-        (match xs with _ :: t ->  r (i+1) t | _ -> failwith "Error") in
-    List.fold_left go (fun _ _ -> []) xs 0 xs
+let update_assoc (k : 'a) (nv : 'b) (l : ('a * 'b) list) : ('a * 'b) list =
+  let go (key, v) acc = if k = key then (key, nv) :: acc else (key, v) :: acc in
+  List.fold_right go [] l
 
 let update (n: int) (x: 'a) (xs: 'a list): 'a list =
-  take n xs @ x :: drop n xs
+  List.mapi (fun i v -> if n = i then x else v) xs
 
 let remove (n: int) (xs: 'a list): 'a list =
-  take n xs @ drop n xs
+  List.filteri (fun i _ -> n = i) xs
 
 (* TASK 1 *)
 (* module CanonicaltreeFlavor : Treeflavor = struct
@@ -176,8 +164,14 @@ module CanonicaltreeFlavor : Treeflavor = struct
        | Some n -> CForm {ckwd = ckwd; cpos = update pos n cpos}
        | _ -> CForm {ckwd = ckwd; cpos = remove pos cpos})
       
-      
-  let update_kval : key:datum -> datum option -> datum -> datum = [%noimplval]
+  let update_kval ~key (nd: datum option) (dat: datum): datum =
+    match dat with
+    | CAtom _ -> dat
+    | CForm {ckwd; cpos} ->
+       (match nd with
+        | Some n -> CForm {ckwd = update_assoc key n ckwd; cpos = cpos}
+        | _ -> CForm {ckwd = List.remove_assoc key ckwd; cpos = cpos})
+  
   let update_root : datum -> datum -> datum = fun _ d -> d
   let update_anno : datum list -> datum -> datum = fun _ dat -> dat
 
@@ -192,6 +186,127 @@ module CanonicaltreeFlavor : Treeflavor = struct
       
 end
 
+module Normaltreeflavor : Treeflavor = struct
+  open Normaltree
+  type datum = Normaltree.ndatum
+  (*
+    
+    | NAtom of atom
+    | NForm of {
+        (* 潰された *)
+        n_keywordeds  : (ndatum, ndatum) assoc;
+        n_positionals : ndatum list;
+        n_annotations : ndatum set;
+      }
+    | NAnnotated of ndatum * ndatum set *)
+      
+  type flavor = ntree
+  let treeflavor : ntree treeflavor = Ntree
+
+  let compare n n' = Canonicaltree.cdatum_ordering (cdatum_of_ndatum n) (cdatum_of_ndatum n')
+  let eqv n n': bool = compare n n' = 0
+  let pp : formatter -> datum -> unit = pp_ndatum
+  let to_string ?pretty:(_=false) = Format.asprintf "%a" pp
+  let datum_of_sexp : sexp -> datum = [%noimplval]
+  let sexp_of_datum : datum -> sexp = sexp_ndatum
+  
+  (** destructors *)
+
+  let atom dat =
+    match dat with
+    | NAtom a -> a
+    | _ -> failwith "Not an atom!"
+             
+  let npos ~pos dat =
+    match dat with
+    | NForm { n_positionals = poses; _} ->
+      List.nth poses pos
+    | _ -> failwith "No positional arguments!"
+  
+  let kval ~key dat =
+    match dat with
+    | NForm { n_keywordeds = kws; _ } ->
+      (match List.assoc_opt key kws with
+       | Some v -> v
+       | None -> failwith "Invalid key!")
+    | _ -> failwith "No keyword arguments!"
+  
+  let root dat =
+    match dat with
+    | NAnnotated (root, _) -> root
+    | _ -> failwith "No annotations!"
+
+  let anno dat =
+    match dat with
+    | NAnnotated (_, anns) -> anns
+    | _ -> failwith "No annotations!"
+  
+  (** constructors & case analyzer *)
+
+  let mkatom : atom -> datum = fun a -> NAtom a
+  let mkform ~keyworded ~positional =
+    NForm {n_keywordeds = keyworded; n_positionals = positional; n_annotations = []}
+  (* val case : atom:(datum -> 'r) ->
+             form:(keyworded:(datum*datum) list ->
+                   positional:datum list ->
+                   'r) ->
+             datum -> 'r*)
+  let rec case ~atom ~form d =
+    match d with
+    | NAtom _ -> atom d
+    | NForm {n_keywordeds = kws; n_positionals = poses ; _} ->
+       form ~keyworded:kws ~positional:poses
+    | NAnnotated (d', _) -> case ~atom ~form d'
+
+  (** updaters *)
+
+  let update_atom a dat =
+    match dat with
+    | NAtom _ -> NAtom a
+    | _ -> dat
+
+  let update_npos ~pos (nd: datum option) (dat: datum): datum =
+    match dat with
+    | NForm {n_keywordeds = kws; n_positionals = poses; n_annotations = anns} ->
+      (match nd with
+       | Some n -> NForm {n_keywordeds = kws;
+                          n_positionals = update pos n poses;
+                          n_annotations = anns}
+       | _ -> NForm {n_keywordeds = kws;
+                     n_positionals = remove pos poses;
+                     n_annotations = anns})
+    | _ -> dat
+
+  let update_kval ~key (nd: datum option) (dat: datum): datum =
+    match dat with
+    | NForm {n_keywordeds = kws; n_positionals = poses; n_annotations = anns} ->
+       (match nd with
+        | Some n -> NForm {n_keywordeds = update_assoc key n kws;
+                           n_positionals = poses;
+                           n_annotations = anns}
+        | _ -> NForm {n_keywordeds = List.remove_assoc key kws;
+                      n_positionals = poses;
+                      n_annotations = anns})
+    | _ -> dat
+      
+  let update_root (nd: datum) (dat: datum): datum =
+    match dat with
+    | NAnnotated (_, anns) -> NAnnotated (nd, anns)
+    | _ -> dat
+  
+  let update_anno (nanns: datum list) (dat: datum): datum =
+    match dat with
+    | NAnnotated (d, _) -> NAnnotated (d, nanns)
+    | _ -> dat
+
+  (** converters *)
+
+  let to_canonicaltree : datum -> ctree = Normaltree.cdatum_of_ndatum
+  let to_normaltree    : datum -> ntree = fun d -> d
+  let to_datatree      : datum -> dtree = Datatree.ddatum_of_ndatum
+  let to_parsetree     : datum -> ptree =
+    composite Parsetree.pdatum_of_ddatum Datatree.ddatum_of_ndatum
+end
 
 module type Zipperlib = functor (Flavor : Treeflavor) -> sig
   type t
