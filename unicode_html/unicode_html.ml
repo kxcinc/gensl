7da9badd1bb51e2a288261bf095dc75b7f9b6543
ekatsym <ekatsymeee@gmail.com>
2021-04-30 16:22:15 +0900

    Added command line option.

--- unicode_html/unicode_html.ml
+++ unicode_html/unicode_html.ml
@@ -1,34 +1,32 @@
 open Sedlex_ppx.Unicode
 open Kxclib
 open Printf
+(* open ArgOptions *)
 
 
-let generate_categories_html () =
+let generate_html lst =
   foldr (fun (name, cset) acc1 ->
       sprintf
         "<table><thead><tr><th>%s</th></tr></thead><tbody>%s</tbody></table>%s"
         name
         (foldr (fun (k, v) acc2 ->
-             sprintf "<tr><td>&#%d;</td><td>&#%d;</td></tr>%s"
-               k v acc2)
+             sprintf "<tr><td>0x%x</td><td>&#%d;</td><td>0x%x</td><td>&#%d;</td></tr>%s"
+               k k v v acc2)
             cset "")
         acc1)
-    Categories.list ""
+    lst ""
 
-let generate_properties_html () =
-  foldr (fun (name, cset) acc1 ->
-      sprintf
-        "<table><thead><tr><th>%s</th></tr></thead><tbody>%s</tbody></table>%s"
-        name
-        (foldr (fun (k, v) acc2 ->
-             sprintf "<tr><td>&#%d;</td><td>&#%d;</td></tr>%s"
-               k v acc2)
-            cset "")
-        acc1)
-    Properties.list ""
-
+let unicode_list lst =
+  List.filter (fun (k, _) -> ArgOptions.has_flag ("-" ^ k)) lst
 
 let () =
-  printf "<h1>Properties.list</h1>%s<h1>Categories.list</h1>%s"
-    (generate_properties_html ())
-    (generate_categories_html ())
+  let categories_list = (generate_html (unicode_list Categories.list)) in
+  let proparties_list = (generate_html (unicode_list Properties.list)) in
+  if categories_list = "" then ()
+  else
+    printf "<h1>Categories.list</h1>%s"
+      (generate_html (unicode_list Categories.list));
+  if proparties_list = "" then ()
+  else
+    printf "<h1>Properties.list</h1>%s"
+      (generate_html (unicode_list Properties.list))
