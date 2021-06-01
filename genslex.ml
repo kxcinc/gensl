dd4ac2bf67ff7b280103be88c2648a715571bb54
Haochen Xie <haochenx@acm.org>
2021-06-01 10:11:56 +0900

    genslex.ml - comment out Lexer.source; just for now

--- genslex.ml
+++ genslex.ml
@@ -147,7 +147,7 @@ module Lexer : Lexer with
   type lexer_error += No_next_valid_token
 
   let loc buf = Sedlexing.lexing_positions buf |> snd (* curr_p *)
-  let source buf = `DirectInput (Some (loc buf).pos_fname)
+  (* let source buf = `DirectInput (Some (loc buf).pos_fname) *)
   let lexer buf =
     try
       let tok = token buf in
