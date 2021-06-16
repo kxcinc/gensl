962d7e72ea06a8ce4467e0508f38f9bdab6bd38f
ekatsym <ekatsymeee@gmail.com>
2021-06-16 16:13:29 +0900

    Changed prompt color to blue.

--- js/gensljs.ml
+++ js/gensljs.ml
@@ -15,9 +15,9 @@ open ParserTypes
  - [ ] make index.html accessible from the Internet via GitHub Pages
 
  - [ ] displayed error message (in red)
- - [ ] modify unparse for CommaSeparator and remove the prefix space
+ - [x] modify unparse for CommaSeparator and remove the prefix space
  - [ ] repl result as if #conv infix then #unparse
- - [ ] change color of prompt
+ - [x] change color of prompt
  - [ ] use monospace font
  - [ ] make input area larger and support multi-line input
  *)
@@ -71,14 +71,26 @@ let () =
   then tryparse Sys.argv.(1) |> debug
   else ()
 
-let add_texts texts =
+let add_paragraph paragraph =
   let repl_history = Dom_html.getElementById "repl-history" in
-  let paragraph = Dom_html.createP Dom_html.document in
-  List.iter
-    (fun text ->
-       Dom.appendChild paragraph (Dom_html.document##createTextNode text))
-    texts;
   Dom.appendChild repl_history paragraph
+
+let add_input text =
+  let p = Dom_html.createP Dom_html.document in
+  let span = Dom_html.createSpan Dom_html.document in
+  let prompt = Dom_html.document##createTextNode (Js.string "gensl> ") in
+  let textnode = Dom_html.document##createTextNode text in
+  Js.Unsafe.set span "style" "color:blue";
+  Dom.appendChild span prompt;
+  Dom.appendChild p span;
+  Dom.appendChild p textnode;
+  add_paragraph p
+
+let add_output text =
+  let p = Dom_html.createP Dom_html.document in
+  let textnode = Dom_html.document##createTextNode text in
+  Dom.appendChild p textnode;
+  add_paragraph p
 
 let clear_input () =
   let input_field = Dom_html.getElementById "input-field" in
@@ -99,8 +111,8 @@ let () =
              begin
                let input_value = Js.Unsafe.get input_field "value" in
                let parsed_value = tryparse (Js.to_string input_value) in
-               add_texts [(Js.string "gensl> "); input_value];
-               add_texts [parsed_value];
+               add_input input_value;
+               add_output parsed_value;
                clear_input ();
                Js._false
              end
